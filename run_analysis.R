47e24eb2ddcf097d31bf6f0e0eee2518603cf15a
Javier Monterrubio <j.monte.m@gmail.com>
2014-10-23 21:54:00 +0200

    Updated script

--- run_analysis.R
+++ run_analysis.R
@@ -1,12 +1,40 @@
-x_train <- read.table("./UCI HAR Dataset/train/X_train.txt",header = F)
-y_train <- read.table("./UCI HAR Dataset/train/y_train.txt",header = F)
-subject_train <- read.table("./UCI HAR Dataset/train/subject_train.txt",header = F)
-
-features <- read.table("./UCI HAR Dataset/features.txt")
-for (i in 1:nrow(features)) {
-  names(x_train)[i] <- paste(features$V2[i])
+readsubdataset <- function(datasettype, directory = "./UCI HAR Dataset/") {
+      #
+      # Reads and combine the data of a data set type in a data.frame
+      #
+      # Args:
+      #   datasettype: train or test
+      #   directory  : base folder for read
+      #
+      # Returns:
+      #   the combined data
+      #
+      filename <- paste(directory,datasettype,"/",sep = "")
+      # Read datasets
+      x_dataset <- read.table(paste(filename,"X_",datasettype,".txt",sep = ""),header = F)
+      y_dataset <- read.table(paste(filename,"y_",datasettype,".txt",sep = ""),header = F)
+      subject_dataset <- read.table(paste(filename,"subject_",datasettype,".txt",sep = ""),header = F)
+      
+      # Read feature names and set to x_dataset
+      features <- read.table(paste(directory,"features.txt",sep = ""))
+      for (i in 1:nrow(features)) {
+            names(x_dataset)[i] <- paste(features$V2[i])
+      }
+      
+      # Set column names to y and subject
+      names(subject_dataset)[1] <- paste("Subject")
+      names(y_dataset)[1] <- paste("Activity")
+      
+      # Change the activity values from the y_dataset with its label name
+      activity_labels <- read.table(paste(directory,"activity_labels.txt",sep = ""),header = F)
+      #for (i in 1:nrow(activity_labels)) {
+      #      y_dataset$Activity[y_dataset$Activity == i] <- activity_labels$V2[i]
+      #}
+      
+      # Finally combine the datasets
+      cbind(x_dataset, y_dataset, subject_dataset)
 }
 
-names(subject_train)[1] <- paste("subject")
-names(y_train)[1] <- paste("label")
-train <- cbind(x_train, y_train, subject_train)
+train <- readsubdataset("train")
+test <- readsubdataset("test")
+dataset <- rbind(train,test)
